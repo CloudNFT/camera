338f6f66d1594075bcd39dbb38a2581eb173c894
avcoin@wallet <84797255+avscan@users.noreply.github.com>
2022-01-17 20:53:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ const [play, pause, screenshot] = buttons;
 const constraints = {
   video: {
     width: {
-      min: 1280,
-      ideal: 1920,
+      min: 1280,   
       max: 2560,
+      ideal: 1920
     },
     height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
+      min: 720,    
+      max: 1440,
+      ideal: 1080
     },
     facingMode: {
       exact: 'environment' //belakang
